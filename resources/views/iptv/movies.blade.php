959f02bd799c7381c585ac7ebbb544127182e232
Mac <chakib.phisique@gmail.com>
2021-04-21 01:17:51 +0000

    make it better

--- resources/views/iptv/movies.blade.php
+++ resources/views/iptv/movies.blade.php
@@ -22,35 +22,15 @@
 <link href="{{ asset('iptvamos/css/style.css') }}" rel="stylesheet"> 
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9TNPRNH1EK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-9TNPRNH1EK');
-</script>
+ 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KKJN8W4');</script>
+ 
 <!-- End Google Tag Manager -->
 <!-- Global site tag (gtag.js) - Google Ads: 398007435 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-398007435"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-398007435');
-</script>
+ 
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKJN8W4"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- Google Tag Manager (noscript) --> 
 <!-- End Google Tag Manager (noscript) -->
 <header class="header">
 <div class="flotte">
