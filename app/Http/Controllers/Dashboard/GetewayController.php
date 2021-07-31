de34efdc75007387c229bf397642b4a59bc72257
Recod96 <chakib.phisique@gmail.com>
2021-07-31 23:37:40 +0100

    mkr

--- app/Http/Controllers/Dashboard/GetewayController.php
+++ app/Http/Controllers/Dashboard/GetewayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Geteway;
 use App\Store;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -24,12 +25,39 @@ class GetewayController extends Controller
 
             $paypal = array(
                 array(
-                    "re-cod.com",
+                    "MOROCCO PAYPAL (chakib.mayen@gmail.com)",
                     "AbS2vbSL9rKbJXruUK2xWgb4yN4Qb7ULNLeJzC2ELWsEfB49xUn92nvQJEnm1giIZFZJY4vAMDkWe6Rf"
                 ),
                 array(
-                    "ora-market.com",
-                    "AR-LyFrg4mz5axs92W5NUYw5VEe_Z5GzJmwyQI6QF9vfYlZGKAng0PoAPciT41J67TxPpb4ZV_36EL_v"
+                    "PPLMAROC 3 (pplmaroc3@outlook.com)",
+                    "AVFpw_weyQPl1mu9499iT678qEguMUGBt-qRX6WtIjd882FRvJYAUtih5oYpuRexaThkOhkRWGn9P8XP"
+                )
+            ,
+                array(
+                    "PPLMAROC 2 (pplmaroc2@outlook.com)",
+                    "AQ2Exy1ssF5qRpK5wt0PYptuIsyjKnmS7NClVVBOwS2quqR2G5G4-5yRGUvOw2gYs2lqd3iLeSsknO6J"
+                )
+            ,
+                array(
+                    "PPLMAROC 7 (pplmaroc7@outlook.com)",
+                    "AUDYODHm4kDZfR8J66MRYkXUpWW8W-F3WDmE7u0E1u0-LkdTquLrKcHiTbJp3ybnf3ngpCpJmcjQj2cG"
+                )
+            ,
+                array(
+                    "UK 1 (howells.kate.97@gmail.com)",
+                    "AevrKMEYncxar9TGOi63wElDkPSHp5rmnnDs9ql5bLnb3qyE1d5IU8rkLBbg6HEON2FsfHv44VyKrpqH"
+                )
+
+            ,
+                array(
+                    "GERMANY 2 PAYPAL (Pplgermany2@outlook.com)",
+                    "AU0Qm0Q_4BGtHVXtANBpB5-DArCZ63M9sue4GecE7eQyD74gmcKTny-QYj6rCYmV-o2n8bmB1EOE8WfE"
+                )
+
+            ,
+                array(
+                    "GERMANY PAYPAL (bobresworld@gmail.com)",
+                    "AUeRUNejh9Q9gX-IpIL5erP792HYAY-KOyveJbl7AXamj5tmoa3WNVCgWNcDGVjmP5iwTCX-4chyJXl_"
                 )
               );
           
@@ -38,9 +66,37 @@ class GetewayController extends Controller
     }
 
     public function paypal_change(Request $request){
-        $store = Store::first(); 
-        $store->unit_system = $request->client_id;  
-        $store->update();
+        $store = Store::first();
+
+        if($request->operation_delete == 'operation_delete') {
+                $mygetGeteway = Geteway::find($request->client_id);
+                $lastgetway = Geteway::all()->last();
+                $mygetGeteway->delete();
+                if($lastgetway->id != $mygetGeteway->id){
+                    $lastgetway->id = $request->client_id;
+                    $lastgetway->update();
+                }
+                $store->unit_system = 1;
+            $store->update();
+            session()->flash('noty_color', 'success');
+            session()->flash('noty_message', __('site.deleted_success'));
+        }
+
+        if($request->operation_create == 'operation_create') {
+            $id = Geteway::all()->last()->id + 10;
+            $getos = Geteway::create();
+            $getos->client_account = $request->name;
+            $getos->api_key = $request->api_key;
+            $getos->mode = "https://www.re-cod.com";
+            $getos->id = $id;
+            $getos->is_active = 1;
+            $getos->store_id = 1;
+            $store->unit_system = 1;
+            $store->update();
+            $getos->update();
+            session()->flash('noty_color', 'success');
+            session()->flash('noty_message', __('site.added_success'));
+        }
         return redirect()->route('settings.paypal');
     }
     
--- resources/views/dashboard/settings/paypal.blade.php
+++ resources/views/dashboard/settings/paypal.blade.php
@@ -34,29 +34,40 @@
     </section>
 
     <section class="content">
+        <div class="card-header">
+            <h3 class="card-title"></h3>
+
+            <div class="card-tools " style="margin-right: 0.475rem;">
+
+
+
+                    <a href="{{ route('categories.create') }} " class="btn btn-default btn-sm " data-toggle="modal" data-target="#exampleModal"
+                       title="@lang('site.add')">
+                        <i class="fas fa-plus"></i> @lang('site.add')</a>
+
+
+            </div>
+
+
+
+        </div>
+
         <div class="container-fluid">
+
             <div class="row">
                 <div class="col-12">
-                    <div class="callout callout-info">
-                        <h5><i class="fas fa-info"></i> @lang('site.note'):</h5>
-                        @lang('site.note_gateway_description')
-                    </div>
 
 
                     <!-- Main content -->
-                    <div class="invoice p-3 mb-3">
+                    <div class="invoice">
                         <div class="card">
 
                              <div class="card-body">
                                 <div class="row">
 
-                                    <div class="col-12 col-md-12 col-lg-4 order-2 order-md-1">
-                                        <h4>  @lang('site.accept_payments')</h4>
-                                        <p class="text-muted">@lang('site.accept_payments_description')</p>
 
-                                    </div>
-                                    <div class="col-12 col-md-12 col-lg-8 order-1 order-md-2">
-                                        <div class="row">
+                                    <div class="col-12">
+
                                     
                             
 
@@ -64,17 +75,22 @@
                                             <thead>
                                                 <tr>
                                                 <th scope="col">#</th>
-                                                <th scope="col">Domain</th> 
-                                                <th scope="col">Action</th>
+                                                    <th scope="col">(Name) Email</th>
+                                                    <th scope="col">API KEY</th>
+                                                    <th scope="col">Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
 
-                                            @foreach($paypal as $index => $pay)
-                                                <tr>
+                                            @foreach($geteways as $index => $pay)
+
+
+                                                <tr class="@if($pay->id == $store->unit_system) table-primary @endif">
                                                 
-                                                <th scope="row">{{$index}}</th>
-                                                <td>{{$pay[0]}}</td>  
+                                                <th scope="row">{{$pay->id}}</th>
+                                                    <td>{{$pay->client_account}}</td>
+                                                    <td>{{$pay->api_key}}</td>
+
                                                 <td>
                                                 
                                                 <form role="form" method="POST" action="{{ route('settings.paypal.change') }}"
@@ -82,11 +98,12 @@
                                                     {{ csrf_field() }}
                                                     {{ method_field('POST') }}
 
-                                                    <input type="hidden" id="client_id" name="client_id" value="{{$pay[1]}}"> 
+                                                    <input type="hidden" id="client_id" name="client_id" value="{{$pay->id}}">
+                                                    <input type="hidden" id="operation_delete" name="operation_delete" value="operation_delete">
 
-                                                    @if($pay[1] != $store->unit_system) 
-                                                    <button type="submit" class="btn btn-danger">Activate</button>
-                                                    @endif
+
+                                                    <button type="submit" class="btn btn-danger">Remove</button>
+
                                                     </form>
                                                 
                                                 </td> 
@@ -97,7 +114,7 @@
                                                
                                             </tbody>
                                             </table>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -111,7 +128,81 @@
     </section>
 
 
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal"  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add PayPal</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form role="form" method="POST" action="{{ route('settings.paypal.change')  }}"
+                          enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        {{ method_field('POST') }}
 
+                        <div class="card-body">
+                            <div class="row">
+                                <input type="hidden" id="operation_delete" name="operation_create" value="operation_create">
+
+                                <div class="col-sm-12">
+                                    <!-- text input -->
+                                    <div class="form-group">
+                                        <label>@lang('site.name')</label>
+                                        <input type="text" class="form-control" name="name"
+                                               value="{{ old('name') }}"
+                                               placeholder="@lang('site.name') ...">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="row">
+
+                                <div class="col-sm-12">
+
+                                    <div class="form-group">
+                                        <label for="exampleInputFile">PayPal</label>
+                                        <select class="form-control" name="api_key">
+                                            @foreach($paypal as $pp)
+                                                <option value="{{$pp[1]}}">{{ $pp[0].'-'. $pp[1] }}</option>
+                                            @endforeach
+                                        </select>
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+
+
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input type="submit"
+                                       class="btn btn-outline-primary btn-block "
+                                       value="@lang('site.add') "/>
+
+
+                            </div>
+                            <!-- /.card-body -->
+                    </form>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <style>
+        .modal-backdrop{
+            z-index: 0 !important;
+        }
+    </style>
 @endsection
 
 <!--JS-->
