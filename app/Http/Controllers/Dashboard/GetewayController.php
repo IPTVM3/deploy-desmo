923cc772c7d6b42ff8c9ef98e5ce02180c895364
Recod96 <chakib.phisique@gmail.com>
2021-10-15 01:04:47 +0100

    make it better

--- app/Http/Controllers/Dashboard/GetewayController.php
+++ app/Http/Controllers/Dashboard/GetewayController.php
@@ -72,14 +72,7 @@ class GetewayController extends Controller
                     "Adana9-DQql8WNnt30S1-CdY2-DNMZnyRt5qZb7IPkHX0uebFaQLkvueNe0VxgiFVt6X0NCsD_2kKOv5"
                 )
 
-
-
-                ,
-                array(
-                    "Paypal21-12 ( pplmaroc21-12@outlook.com )",
-                    "AQ7xkv820azL4y1DN2g2UEfEhYT5vDQJ3bFgwzbpdbnOnfhBwrHeGtNoX5mZFLoMomrE9HdqpgAIt1Wi"
-                )
-
+ 
 
                 ,
                 array(
