75ab7e4b70e1cffdad5873354dddf07809a0985d
CHAKIB MAYEN <recod@CHAKIBs-MBP.Home>
2021-11-26 18:08:28 +0100

    make it better

--- resources/views/iptv/condition.blade.php
+++ resources/views/iptv/condition.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>@lang('amos.conditions')</title>
+    <title>@lang('amos.conditions')...</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
