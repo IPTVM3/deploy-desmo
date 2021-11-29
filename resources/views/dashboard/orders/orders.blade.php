2006bcf580247a9d19e793348bf12bfed9afb5e8
recod <chakib.phisique@gmail.com>
2021-11-29 16:52:57 +0100

    make iur better

--- resources/views/dashboard/orders/orders.blade.php
+++ resources/views/dashboard/orders/orders.blade.php
@@ -119,6 +119,28 @@
 </style>
 	</head>
 	<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Bobres</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/dashboard">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/orders/new">Orders</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </nav>
+
+
 	<section class="ftco-section">
 		<div class="container">
 
