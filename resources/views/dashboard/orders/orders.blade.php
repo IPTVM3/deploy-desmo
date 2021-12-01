1948a44bd2f9cde2e506fb565cf8d30357977946
recod <chakib.phisique@gmail.com>
2021-12-01 18:40:18 +0100

    make it better

--- resources/views/dashboard/orders/orders.blade.php
+++ resources/views/dashboard/orders/orders.blade.php
@@ -116,6 +116,87 @@
         .action > div .btn {
             border-radius: 70px !important;
         }
+
+        body.modal-open {
+      padding-right: 0 !important;
+    }
+
+    #sidebar {
+      padding-left: 0;
+    }
+    /*
+ * Off Canvas at medium breakpoint
+ * --------------------------------------------------
+ */
+
+    @media screen and (max-width: 48em) {
+      .row-offcanvas {
+        position: relative;
+        -webkit-transition: all 0.25s ease-out;
+        -moz-transition: all 0.25s ease-out;
+        transition: all 0.25s ease-out;
+      }
+      .row-offcanvas-left .sidebar-offcanvas {
+        left: -33%;
+      }
+      .row-offcanvas-left.active {
+        left: 33%;
+        margin-left: -6px;
+      }
+      .sidebar-offcanvas {
+        position: absolute;
+        top: 0;
+        width: 33%;
+        height: 100%;
+      }
+    }
+    /*
+ * Off Canvas wider at sm breakpoint
+ * --------------------------------------------------
+ */
+
+    @media screen and (max-width: 34em) {
+      .row-offcanvas-left .sidebar-offcanvas {
+        left: -45%;
+      }
+      .row-offcanvas-left.active {
+        left: 45%;
+        margin-left: -6px;
+      }
+      .sidebar-offcanvas {
+        width: 45%;
+      }
+    }
+
+    .card {
+      overflow: hidden;
+    }
+
+    .card-block .rotate {
+      z-index: 8;
+      float: right;
+      height: 100%;
+    }
+
+    .card-block .rotate i {
+      color: rgba(20, 20, 20, 0.15);
+      position: absolute;
+      left: 0;
+      left: auto;
+      right: -10px;
+      bottom: 0;
+      display: block;
+      -webkit-transform: rotate(-44deg);
+      -moz-transform: rotate(-44deg);
+      -o-transform: rotate(-44deg);
+      -ms-transform: rotate(-44deg);
+      transform: rotate(-44deg);
+    }
+
+    .display-1{
+        font-size: 2rem;
+        color:black;
+    }
 </style>
 	</head>
 	<body>
@@ -146,6 +227,55 @@
 	<section class="ftco-section">
 		<div class="container">
 
+   
+  <div class="row mb-3">
+          <div class="col-xl-3 col-lg-6">
+            <div class="card card-inverse card-success">
+              <div class="card-block bg-success">
+                <div class="rotate">
+                  <i class="fa fa-user fa-5x"></i>
+                </div>
+                <h6 class="text-uppercase">Today</h6>
+                <h1 class="display-1" id="todayOrders"></h1>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-6">
+            <div class="card card-inverse card-danger">
+              <div class="card-block bg-danger">
+                <div class="rotate">
+                  <i class="fa fa-list fa-4x"></i>
+                </div>
+                <h6 class="text-uppercase">Yestrday</h6>
+                <h1 class="display-1" id="lastDayOrders"></h1>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-6">
+            <div class="card card-inverse card-info">
+              <div class="card-block bg-info">
+                <div class="rotate">
+                  <i class="fa fa-twitter fa-5x"></i>
+                </div>
+                <h6 class="text-uppercase">Today</h6>
+                <h1 class="display-1" id="today_visitor"></h1>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-6">
+            <div class="card card-inverse card-warning">
+              <div class="card-block bg-warning">
+                <div class="rotate">
+                  <i class="fa fa-share fa-5x"></i>
+                </div>
+                <h6 class="text-uppercase">Yestrday</h6>
+                <h1 class="display-1"  id="yestardy_visitor"></h1>
+              </div>
+            </div>
+          </div>
+        </div>
+ 
+ 
 
 
 
--- resources/views/dashboard/overview.blade.php
+++ resources/views/dashboard/overview.blade.php
@@ -213,8 +213,7 @@
 
     <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light" style="float:left !important;">
-    <a class="navbar-brand" href="#">Bobres</a>
-
+ 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
