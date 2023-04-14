8a2fdfc8c8bcb76012bd07eca1829709d93f487b
lena jacker <contact.bobres@gmail.com>
2023-04-14 16:16:28 +0000

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        Schema::defaultStringLength(191);
+    {  
+       
+            \URL::forceScheme('https');
+        
     }
 
     /**
