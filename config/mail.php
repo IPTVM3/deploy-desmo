584e4b618c80dc6268f22ddfb0c3b91e3b22fb25
mayen mobarak <mobarak.mayen@gmail.com>
2023-06-20 23:43:00 +0200

    make it better

--- config/mail.php
+++ config/mail.php
@@ -11,7 +11,7 @@ return [
     ],
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     'username'   => 'iptv21m3u@gmail.com',
-    'password'   => 'qyqcxardyuddvwvi',
+    'password'   => 'ehdwmwzyskoskjcz',
     'sendmail'   => '/usr/sbin/sendmail -bs',
 
     'markdown' => [
