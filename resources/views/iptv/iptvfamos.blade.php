3e0bd90977a8a5128b299cb62e8959cfce7d9765
mayen mobarak <mobarak.mayen@gmail.com>
2023-08-08 19:38:24 +0100

    make it better

--- resources/views/iptv/iptvfamos.blade.php
+++ resources/views/iptv/iptvfamos.blade.php
@@ -2178,10 +2178,8 @@ element.style {
 <div class="container-fluid">
 <div class="row">
 <div class="col-lg-12 text-center text-white">
-<h2 style="    color: gold !important;" class="parallex-heading text-center font-weight-bold text-white pb-20 pt-3">Special 4K / FHD and PPV Events</h2>
-
-
-<div  style=" background-color: #ffffff12;"  class="form-row text-center" >
+<
+				<div  style=" background-color: #ffffff12;"  class="form-row text-center" >
                     <div class="col-md-4"  >
                         <select id="package" class="form-control" name="package" style="font-weight: 800">
                             <option disabled>@lang('new.multidevice_option1')</option>
@@ -2208,40 +2206,17 @@ element.style {
                             <option selected value="12">1 @lang('iptv.yr')</option>
                         </select>
                     </div>
-
-                </div>
-				<br>
-				<br>
-				<br>
-
-			 
-				<a style="height: 56px; 
-    line-height: 52px;
-    padding: 0 40px;
-    display: inline-block;
-    color: #fff;
-    background: #8a39ee;
-    font-weight: 700;
-    border-radius: 56px;
-    box-shadow: 0 20px 30px rgb(0 0 0 / 5%);
-    border: 2px solid transparent;
-    margin: 12px 0;" target="_blank" id="pricevalueMM" href="/contact">@lang('new.multidevice_desc_button') - <span id="pricevalue" style="color:orange;"></span> </a>
-						 
-		 
-								</div>
-  
-		</div>
-		<end row -->
-	   
-
-
-	</section >
-<br>
+					<br>
 <a href="" style="font-style: ; font-variant: normal; font-stretch: ; font-size: ; line-height: ; font-family: ; font-optical-sizing: ; font-kerning: ; font-feature-settings: ; font-variation-settings: ; font-weight: bold; color: black; background: gold !important; visibility: visible;" class="custom-btn green hvr-shutter-out-horizontal-blk wow fadeInUp btn btn-large btn btn-medium btn-rounded animated animated">Get It Now! - 79 euros</a>
 </div>
 </div>
 </div>
 </section>
+
+
+
+
+
 
 
 
@@ -2314,33 +2289,6 @@ a, *::before, *::after, img, span, input, button, .navbar, .fixedmenu, .tp-bulle
  
 
 
-				<div  style=" background-color: #ffffff12;"  class="form-row text-center" >
-                    <div class="col-md-4"  >
-                        <select id="package" class="form-control" name="package" style="font-weight: 800">
-                            <option disabled>@lang('new.multidevice_option1')</option>
-                            <option selected value="59">@lang('iptv.prim')</option> 
-                            
-                        </select>
-                    </div>
-                
-                    <div class="col-md-4">
-                        <select id="multidevicex" class="form-control" name="multidevicex" style="font-weight: 800">
-                            <option disabled>@lang('new.multidevice_option2')</option>
-                            <option selected value="2">2 @lang('new.device')</option>
-                            <option value="3">3 @lang('new.device')</option>
-                            <option value="4">4 @lang('new.device')</option>
-                            <option value="5">5 @lang('new.device')</option>
-                            <option value="6">6 @lang('new.device')</option>
-                        </select>
-                    </div>
-                    <div class="col-md-4">
-                        <select id="periods" class="form-control" name="periods" style="font-weight: 800;">
-                            <option disabled>@lang('new.multidevice_option3')</option>
-                            <option value="3">3 @lang('iptv.mounth')</option>
-                            <option value="6">6 @lang('iptv.mounth')</option>
-                            <option selected value="12">1 @lang('iptv.yr')</option>
-                        </select>
-                    </div>
 
                 </div>
 				<br>
