b506fd3d6f966d58a560d61b9c7f1d3eaf25729c
mayen mobarak <mobarak.mayen@gmail.com>
2023-08-08 19:41:21 +0100

    make it better

--- resources/views/iptv/iptvfamos.blade.php
+++ resources/views/iptv/iptvfamos.blade.php
@@ -2178,56 +2178,13 @@ element.style {
 <div class="container-fluid">
 <div class="row">
 <div class="col-lg-12 text-center text-white">
-<
-				<div  style=" background-color: #ffffff12;"  class="form-row text-center" >
-                    <div class="col-md-4"  >
-                        <select id="package" class="form-control" name="package" style="font-weight: 800">
-                            <option disabled>@lang('new.multidevice_option1')</option>
-                            <option selected value="59">@lang('iptv.prim')</option> 
-                            
-                        </select>
-                    </div>
-                
-                    <div class="col-md-4">
-                        <select id="multidevicex" class="form-control" name="multidevicex" style="font-weight: 800">
-                            <option disabled>@lang('new.multidevice_option2')</option>
-                            <option selected value="2">2 @lang('new.device')</option>
-                            <option value="3">3 @lang('new.device')</option>
-                            <option value="4">4 @lang('new.device')</option>
-                            <option value="5">5 @lang('new.device')</option>
-                            <option value="6">6 @lang('new.device')</option>
-                        </select>
-                    </div>
-                    <div class="col-md-4">
-                        <select id="periods" class="form-control" name="periods" style="font-weight: 800;">
-                            <option disabled>@lang('new.multidevice_option3')</option>
-                            <option value="3">3 @lang('iptv.mounth')</option>
-                            <option value="6">6 @lang('iptv.mounth')</option>
-                            <option selected value="12">1 @lang('iptv.yr')</option>
-                        </select>
-                    </div>
-					br>
+<h2 style="    color: gold !important;" class="parallex-heading text-center font-weight-bold text-white pb-20 pt-3">Special 4K / FHD and PPV Events</h2>
+<br>
 <a href="" style="font-style: ; font-variant: normal; font-stretch: ; font-size: ; line-height: ; font-family: ; font-optical-sizing: ; font-kerning: ; font-feature-settings: ; font-variation-settings: ; font-weight: bold; color: black; background: gold !important; visibility: visible;" class="custom-btn green hvr-shutter-out-horizontal-blk wow fadeInUp btn btn-large btn btn-medium btn-rounded animated animated">Get It Now! - 79 euros</a>
 </div>
 </div>
 </div>
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <section id="trial" class="parallex-section parallax parallaxie img-size cover-img-trial" style="margin-top: 1px; margin-bottom: 1px; background-image: none; background-size: cover; background-repeat: no-repeat; background-attachment: fixed; background-position: center 174.647px;">
@@ -2289,6 +2246,33 @@ a, *::before, *::after, img, span, input, button, .navbar, .fixedmenu, .tp-bulle
  
 
 
+				<div  style=" background-color: #ffffff12;"  class="form-row text-center" >
+                    <div class="col-md-4"  >
+                        <select id="package" class="form-control" name="package" style="font-weight: 800">
+                            <option disabled>@lang('new.multidevice_option1')</option>
+                            <option selected value="59">@lang('iptv.prim')</option> 
+                            
+                        </select>
+                    </div>
+                
+                    <div class="col-md-4">
+                        <select id="multidevicex" class="form-control" name="multidevicex" style="font-weight: 800">
+                            <option disabled>@lang('new.multidevice_option2')</option>
+                            <option selected value="2">2 @lang('new.device')</option>
+                            <option value="3">3 @lang('new.device')</option>
+                            <option value="4">4 @lang('new.device')</option>
+                            <option value="5">5 @lang('new.device')</option>
+                            <option value="6">6 @lang('new.device')</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4">
+                        <select id="periods" class="form-control" name="periods" style="font-weight: 800;">
+                            <option disabled>@lang('new.multidevice_option3')</option>
+                            <option value="3">3 @lang('iptv.mounth')</option>
+                            <option value="6">6 @lang('iptv.mounth')</option>
+                            <option selected value="12">1 @lang('iptv.yr')</option>
+                        </select>
+                    </div>
 
                 </div>
 				<br>
