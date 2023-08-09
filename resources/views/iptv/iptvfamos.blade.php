7f9ad5a6c2f0e36350acd95dcd48338f9e1f740e
mayen mobarak <mobarak.mayen@gmail.com>
2023-08-09 01:17:39 +0100

    make it better

--- resources/views/iptv/iptvfamos.blade.php
+++ resources/views/iptv/iptvfamos.blade.php
@@ -2180,7 +2180,6 @@ element.style {
     to bottom,
     rgb(255 255 0 / 29%), rgb(234 240 241 / 7%)
   ), url(https://www.mystarr.de/iptv_update/images/b3.jpg);  background-size: cover; background-repeat: no-repeat; bottom: 33px; ">
-<section id="trial" class="parallex-section parallax parallaxie img-size cover-img"style="margin-top: 1px;margin-bottom: 1px;" >
 <div class="container-fluid">
 <div class="row">
 <div class="col-lg-12 text-center text-white">
